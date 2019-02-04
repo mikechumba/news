00f18b6b3620caf2d25965146d54a2cadec62b0a
Michael Kipchumba <michaelchumba09@gmail.com>
2019-02-04 11:40:05 +0300

    change sass compiler

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from scss import Scss
 
+from scss import Compiler
+Compiler().compile_string("a { color: red + green; }")
+
 app = Flask(__name__)
 
 if __name__=='__main__':
