16670cd586f80e097b554ff5e16b74f2adf9cdb1
Michael Kipchumba <michaelchumba09@gmail.com>
2019-02-04 17:52:59 +0300

    remove port specification in run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 import os
 
-port = int(os.environ.get('PORT', 5000))
+# port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-  app.run(debug=True,port=port)
+  app.run(debug=True)
